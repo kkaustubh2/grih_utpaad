386da012792d139b7e50bee8abddc321a29d7e5c
kkaustubh2 <723146521q@gmail.com>
2025-05-22 23:44:26 +0530

    Done

--- grih-utpaad/config/db.php
+++ grih-utpaad/config/db.php
@@ -10,8 +10,8 @@ $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
 // Set charset to utf8mb4
-$conn->set_charset("utf8mb4"); 
+$conn->set_charset("utf8mb4");
